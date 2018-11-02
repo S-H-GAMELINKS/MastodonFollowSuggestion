840d1c8e40900cc2fbcbd676d45e8938e5f3473f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-02 13:41:13 +0900

    add userLogin

--- app/javascript/packs/components/web/Index.vue
+++ app/javascript/packs/components/web/Index.vue
@@ -1,5 +1,47 @@
 <template>
 <div class="container">
-    <h1>Index</h1>
+    <div v-if="user_login">
+    </div>
+    <div v-else>
+        <h1>Index</h1>
+    </div>
 </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data: function() {
+        return {
+            user_login: false,
+            userName: "",
+            userIcon: "",
+            userDomain: "",
+            recoUsers: [],
+        }
+    },
+    mounted: function() {
+        this.userLogin()
+    },
+    methods: {
+        userLogin: function() {
+            axios.get('/api/sessions/login').then((response) => {
+                console.log(response);
+                if(response.data === null) {
+                    this.user_login = false;
+                } else {
+                    this.user_login = true;
+                    this.userName = response.data.uid;
+                    this.userIcon = response.data.icon;
+                    this.userDomain = response.data.domain;
+                }
+                console.log(this.userDomain);
+            }, (error) => {
+                console.log(error);
+            })
+        }
+    }
+
+}
+</script>
