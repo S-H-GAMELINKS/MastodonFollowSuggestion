d7f96b7138c279b9a87211022d37ae28b0d90692
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-02 14:03:10 +0900

    using token & domain

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,17 @@ class User < ApplicationRecord
 
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    puts auth
       
     unless user
       user = User.create(
         uid:      auth.uid,
         provider: auth.provider,
         email:    User.dummy_email(auth),
-        password: Devise.friendly_token[0, 20]
+        password: Devise.friendly_token[0, 20],
+        domain: User.get_domain(auth),
+        token: auth.credentials.token
       )
       user.save!
     end
@@ -23,5 +27,9 @@ class User < ApplicationRecord
       
   def self.dummy_email(auth)
     "#{auth.uid}"
+  end
+
+  def self.get_domain(auth)
+    auth.uid.to_s.gsub(/#{auth.info.name.to_s.downcase}\@/, "")
   end
 end
