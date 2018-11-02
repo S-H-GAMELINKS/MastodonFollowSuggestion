56950c68326e73fe1c516f4f4eacad6a2274c75c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-02 11:29:38 +0900

    using Header.vue

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,6 +3,9 @@ import Header from './components/layouts/Header.vue';
 
 const app = new Vue({
     el: '#app',
+    components: {
+        'nav-bar': Header
+    },
     data: function() {
         return {
             mes: "hello"
