043e06303d12554ba4304ca7d8985f306545a4a0
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-02 11:50:24 +0900

    using SPA router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   root 'web#index'
+
+  get '/',   to: 'web#index'
+  get '/about',   to: 'web#index'
+  get '/contact',   to: 'web#index'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   get '/api/sessions/login', to: 'api/sessions#user_session'
